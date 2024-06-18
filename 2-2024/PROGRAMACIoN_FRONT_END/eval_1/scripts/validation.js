ea85e1ba158e11601c812c56f642744946f3dbab
jilguerillo <mate_lg@tutanota.com>
2024-06-17 22:20:37 -0400

    add: evaluation 1 front}

---
+++ 2-2024/PROGRAMACIoN_FRONT_END/eval_1/scripts/validation.js
@@ -0,0 +1,9 @@
+// scripts/validacion.js
+
+function validarFormulario() {
+ 
+    return true;
+}
+
+const botonEnviar = document.getElementById('enviar');
+botonEnviar.addEventListener('click', validarFormulario);
